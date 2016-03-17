99cff5030a70e569ee49aec2a6c3c5a439f44212
unknown <roundyz@roundyz.kicks-ass.net>
2016-03-17 15:21:13 +0000

    working though dates

--- CommonFunctions.php
+++ CommonFunctions.php
@@ -7,6 +7,7 @@ class CommonFunctions
 
 
 	public $gtinCheckDigit;
+	public $gtinWithCheckDigit;
 
 
 	// calcuate gtin check digit for either 7, 11, 12 or 13 long int
@@ -37,6 +38,7 @@ class CommonFunctions
 			$nearestTen = round($sum, -1);
 			$gtinCheckDigit = $nearestTen - $sum;
 			$this->gtinCheckDigit = $gtin . $gtinCheckDigit;
+			$this->gtinWithCheckDigit = $gtin . $gtinCheckDigit;
 			if ($gtinCheckDigit > 0) {
 				$ret = true;
 			}
@@ -44,12 +46,73 @@ class CommonFunctions
 		return $ret;
 	}
 
+
+private $canDeliverySaturday = false;
+private $canDeliverySunday = false;
+private $ecomonyDays = 2;
+
+public $nextDayDelivery = null;
+public $ecomonyDayDelivery = null;
+public $dates = [
+'Monday',
+'Tuesday',
+'Wednesday',
+'Thursday',
+'Friday',
+'Saturday',
+'Sunday'
+];
+
 /* can only deliver on working days */
 /* saturday and sunday are not working days */
 /* but are for partners */
+/* assumed date is a validate Date class */
 	public function calculateDeliveryDates($date = null)
 	{
 		$ret = false;
+		if ($date !== null) {
+			//TODO calc next working day
+			$this->nextDayDelivery = $this->determineNextDate($date, 0);
+
+			//TODO fix this next ecomony day
+			$this->ecomonyDayDelivery = $this->determineNextDate($date, $this->ecomonyDays);
+			$ret = true;
+		}
 		return $ret;
 	}
+
+
+	private function determineNextDate($date, $offset = 0)
+	{
+		$currentDay = $date->format('l');
+		$ret = $date;
+		$nextWorkingDay = array_search($currentDay, $this->dates);
+		// will cover the weekend
+		if ($nextWorkingDay >= 4) {
+			if ($nextWorkingDay == 4) { // friday
+				if ($this->canDeliverySaturday) {
+					$ret = $date->add(DateInterval::createFromDateString('+'. (1+ $offset) .' day'));
+				} elseif ($this->canDeliverySunday) {
+					$ret = $date->add(DateInterval::createFromDateString('+' . (2 + $offset) .'days'));
+				} else {
+					$ret = $date->add(DateInterval::createFromDateString('+' . (3 + $offset) .'days'));
+				}
+			}
+			if ($nextWorkingDay == 5) { //saturday
+				if ($this->canDeliverySunday) {
+					$ret = $date->add(DateInterval::createFromDateString('+'. (1+ $offset) .' day'));
+				} else {
+					$ret = $date->add(DateInterval::createFromDateString('+' . (2 + $offset) .'days'));
+				}
+			}
+			if ($nextWorkingDay == 6) { //sunday
+				$ret = $date->add(DateInterval::createFromDateString('+'. (1+ $offset) .' day'));
+			}
+		} else {
+
+			$ret = $date->add(DateInterval::createFromDateString('+'. (1+ $offset) .' day'));
+		}
+		return $ret;
+	}
+
 }
--- run.php
+++ run.php
@@ -1,11 +1,24 @@
 <?php
 include_once "CommonFunctions.php";
 
-$cf = new CommonFunctions();
 
+//gtin check
+$cf = new CommonFunctions();
 $gtin13 = 629104150021;
 $res = $cf->calcuateGTINCheckDigit($gtin13);
+/* if ($res) { */
+/* 	printf("Gtin-13 %s : with check digit: %s", $gtin13, $cf->gtinWithCheckDigit); */
+/* } */
 
+
+//calc next day delivery
+$cf = new CommonFunctions();
+$date = new DateTime();
+$res = $cf->calculateDeliveryDates($date);
+$format = 'D-m-y';
 if ($res) {
-	printf("Gtin-13 %s : with check digit: %s", $gtin13, $cf->gtinCheckDigit);
+	$a = $date->format($format);
+	$b = $cf->nextDayDelivery->format($format);
+	$c = $cf->ecomonyDayDelivery->format($format);
+	printf("Day = %s, nextDay = %s, eco= %s", $a, $b, $c);
 }
